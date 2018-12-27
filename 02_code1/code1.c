2e6352b998d88078cd5a767550356959237d1e2f
Coursera Learner <nobody@nowhere.nul>
2018-12-27 10:01:17 +0000

    Did code1 of assignment

--- 02_code1/code1.c
+++ 02_code1/code1.c
@@ -1,4 +1,3 @@
-#include<stdlib.h>
 int max (int num1, int num2) {
   if (num1>num2){
     printf("your answer is %d", num1);
@@ -12,8 +11,7 @@ int main(void) {
   printf("max(42, -69) is %d\n", max(42, -69));
   printf("max(33, 0) is %d\n", max(33, 0));
   printf("max(0x123456, 123456) is %d\n", max(0x123456, 123456));
-  int a=max(0x451215AF, 0x913591AF);
-  printf("max(0x451215, 0x913519AF) is %d\n", a);
+  printf("max(0x451215AF, 0x913591AF) is %d\n", max(0x451215, 0x913519AF));
   return 0;
 }
 
