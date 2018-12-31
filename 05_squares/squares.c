9e725f6d7515e6d017f3fe2a08c3acbbea53e340
Coursera Learner <nobody@nowhere.nul>
2018-12-31 13:28:06 +0000

    Did assignment 05_squares

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,16 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+int max(int a, int b)
+{
+  if (a>b)
+    {
+      return a;
+    }
+  else
+    {
+      return b;
+    }
+}
 
+int isBetween(int num, int lower, int upper)
+{
+  
+  if (num >= lower && num < upper)
+    {
+    return 1;
+    }
+  else
+    {    
+    return 0;
+    }
+  
+}
 
-void squares(int size1, int x_offset, int y_offset, int size2) {
+void squares(int size1, int x_offset, int y_offset, int size2)
+{
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int w=max(size1,(x_offset + size2));
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h=max(size1,(y_offset +size2));
+  int y=0;
+  int x=0;
   //count from 0 to h. Call the number you count with y
-
+  for(y=0; y<h; y++)
+    {
     //count from 0 to w. Call the number you count with x
-
+    for (x=0; x<w; x++)
+      {
+      if ((isBetween(x, x_offset, x_offset + size2) && (y == y_offset || y == y_offset + size2 - 1)) || (isBetween(y, y_offset, y_offset + size2) && (x == x_offset || x == x_offset + size2 - 1)))
+	
+	{
+	  
+	  printf("*");
+	  
+	}
+    
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -18,16 +55,36 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-
-      //if not,
+      else if ((x < size1 && (y == 0 || y == size1 - 1)) || (y < size1 && (x == 0 || x == size1 - 1)))
+	
+	{
+	  
+	  printf("#");
+	  
+	}
+      
+    //if not,
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
+      else
+	
+	{
+	  
+	  printf(" ");
+	  
+	}
+      
+      }
+    
+    printf("\n");
+    
+    }
+  
+}
 
       //else print a space
     //when you finish counting x from 0 to w, 
     //print a newline
-
-}
