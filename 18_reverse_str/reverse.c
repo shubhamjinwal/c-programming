eefa21aa94dd69286d9bbaab70af3da3a83f74c3
Coursera Learner <nobody@nowhere.nul>
2019-01-22 16:30:16 +0000

    Did assignment 18_reverse_str

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -1,23 +1,73 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
 
+
+
 void reverse(char * str) {
-  //WRITE ME!
+  
+  if (str == NULL) {
+    
+    return;
+    
+  }
+  
+  int len = strlen(str);
+  
+  if (len < 2) {
+    
+    return;
+    
+  }
+  
+  char temp[len + 1];
+  
+  strncpy(temp, str, len + 1);
+  
+  char* t = &temp[len - 1];
+  
+
+  
+  for (int i = 0; i < len; i++) {
+    
+    *str++ = *t--;
+    
+  }
+  
+  *str = 0;
+  
 }
 
+
+
 int main(void) {
+  
   char str0[] = "";
+  
   char str1[] = "123";
+  
   char str2[] = "abcd";
+  
   char str3[] = "Captain's log, Stardate 42523.7";
+  
   char str4[] = "Hello, my name is Inigo Montoya.";
+  
   char str5[] = "You can be my wingman anyday!";
+  
   char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
+  
   for (int i = 0; i < 7; i++) {
+    
     reverse(array[i]);
+    
     printf("%s\n", array[i]);
+    
   }
+  
   return EXIT_SUCCESS;
+  
 }
